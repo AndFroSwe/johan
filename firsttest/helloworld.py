9dba06bd74fd03d5830a89ee418f9dc579ab3253
unknown <froderberg89@gmail.com>
2019-03-10 01:58:51 -0800

    Lord program

--- firsttest/helloworld.py
+++ firsttest/helloworld.py
@@ -1,1 +1,16 @@
-print("hello World")
+def user_name(name,surname):
+	#outname =  surname+", "+ name 
+	outname = "Lord " + name+ " of "+ surname
+	return outname
+
+def main():		
+	name = input("give name: ")
+	surname = input("give surname: ")
+	fullname = user_name(name,surname)
+	print(fullname)
+
+
+
+main()
+
+
